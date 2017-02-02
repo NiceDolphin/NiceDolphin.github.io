d9be257fc67f58451652ae87946d5438288f7a45
teddygum.jr <teddygum.jr@gmail.com>
2017-02-01 23:18:52 -0500

    addpages

--- myscripts.js
+++ myscripts.js
@@ -18,3 +18,6 @@ function getNumbers(){
       document.getElementById("results").innerHTML="Wrong!";
     }
   }
+var pics = ["https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwic9r_zxPDRAhXCrFQKHTkfAnEQjRwIBw&url=http%3A%2F%2Fnintendoenthusiast.com%2Fforums%2Fthreads%2Fidea-for-shrek-dlc-in-pokemon.3919%2F&psig=AFQjCNFRVRgPzB9wWlA3g7lkYh3nLD26_w&ust=1486094738902636
+            "https://s-media-cache-ak0.pinimg.com/736x/f5/f7/f2/f5f7f2b5504b10314f5b066084617a5f.jpg"
+            "http://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/getty/article/141/245/stk26105dcs.jpg"
